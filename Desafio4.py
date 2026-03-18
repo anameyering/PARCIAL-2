c98d08917ee12315485030829de3983e7c1455cc
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-18 08:53:08 -0300

    Desafio4.py

--- Desafio4.py
+++ Desafio4.py
@@ -1,14 +1,14 @@
+#Calculadora simples
 n1 = float(input("selecione um número: "))
 n2 = float(input("selecione outro numero: "))
 operação = input("escolha uma opração [soma, subtração, multiplicação ou divisão]: ")
-
 if operação == "soma":
-    print(n1 + n2)
+ print( n1 + n2)
 elif operação == "subtração":
-    print(n1 - n2)
+ print( n1 - n2)
 elif operação == "multiplicação":
-    print(n1 * n2)
+ print( n1 * n2)
 elif operação == "divisão":
-    print(n1 / n2)
+ print( n1 / n2)
 else:
-    print("essa opração não existe")
+ print("essa opração não existe")
