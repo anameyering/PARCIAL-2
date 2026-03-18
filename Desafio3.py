983ed629a22280022b1edace6282e8e50d3b9728
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-18 08:37:30 -0300

    Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,9 +1,5 @@
-#LISTA DE NOMES
-n1 = float(input('Informe nome 1: '))
-n2 = float(input('Informe nome 2: '))
-n3 = float(input('Informe nome 3: '))
-n4 = float(input('Informe nome 4: '))
-n5 = float(input('Informe nome 5: '))
+#LISTA NOMES
+Nomes = ['Ana','Gabriela','Helena','Vanessa','Eduarda' ]
 
-nomes = ["n1", "n2", "n3", "n4", "n5"]
+print(Nomes)
 
