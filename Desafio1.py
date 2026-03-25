3e7e375fe9b4665d849d80e689892ce23cfd37f5
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-25 08:44:29 -0300

    Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,6 +1,6 @@
 #SOMA DE NÙMEROS
 n1 = float(input('Informe número 1: '))
 n2 = float(input('Informe número 2: '))
-soma = n1 + n2
+Soma = n1 + n2
 
-print(n1, '+', n2, '=', soma) 
+print(n1, '+', n2, '=', soma)
