44354506c04decc42a87b37af96606fe48c8a78d
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-25 08:50:29 -0300

    Desafio2.py

--- Desafio2.py
+++ Desafio2.py
@@ -1,6 +1,6 @@
 #NUMERO PAR OU IMPAR
-numero = int(input("Digite um número: "))
-if numero % 2 == 0:
-    print(f"O número {numero} é par.")
+Número = int(input("Digite um número: "))
+if Número % 2 == 0:
+    print(fO número {Número} é par.")
 else:
-    print(f"O número {numero} é ímpar.")
+    print(fO número {Número} é ímpar.")
