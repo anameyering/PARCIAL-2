5cfab297185b32da921d596fd5eba2e0efa1005f
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-26 22:34:05 -0300

    Desafio6.py

--- Desafio6.py
+++ Desafio6.py
@@ -1,20 +1,32 @@
 #Tempo
+
+#Mostra a mensagem e espera o usuário digitar algo.
 segundos = int(input("Digite a quantidade de segundos: "))
 
+#Calcula quantas horas completas cabem no total de segundos, dividindo segundos por 3600, pois, 1 hora=3600 segundos.
 horas = segundos // 3600
+
+#Aqui, calcula quantos segundos sobram depois de tirar as horas completas.
 resto = segundos % 3600
 
+#Calcula qauntos minutos cabe no resto.
 minutos = resto // 60
+
+#Calcula quantos segundos ainda sobram depois dos minutos.
 segundos_restantes = resto % 60
 
+#Mostra na tela as horas, minutos e segundos calculados.
 print("Horas:", horas)
 print("Minutos:", minutos)
 print("Segundos:", segundos_restantes)
 
+#Nas 3 linhas seguintres, pergunta ao usuário as horas, minutos e segundos separadamente.
 horas = int(input("Digite as horas: "))
 minutos = int(input("Digite os minutos: "))
 segundos = int(input("Digite os segundos: "))
 
-total_segundos = horas * 3600 + minutos * 60 + segundos
+#Converte tudo para segundos.
+Total segundos = horas * 3600 + minutos * 60 + segundos
 
+#Mostra na tela o total de segundos calculado.
 print("Total em segundos:", total_segundos)
