5a30e255ea13839f04872261e63d3f621e91fc98
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-26 21:47:11 -0300

    Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,6 +1,11 @@
 #SOMA DE NÙMEROS
+
+#As duas linhas seguintes mostra uma mensagem para o usuário informar um número e aguarda a resposta.
 n1 = float(input('Informe número 1: '))
 n2 = float(input('Informe número 2: '))
+
+#Faz a soma dos dois números informados.
 Soma = n1 + n2
 
+#mostra a resposta da soma na tela.
 print(n1, '+', n2, '=', Soma)
