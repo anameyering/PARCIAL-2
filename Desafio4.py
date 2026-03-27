7619af46e6ee7fb3808d600a68143b2e84f31765
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-26 22:35:33 -0300

    Desafio4.py

--- Desafio4.py
+++ Desafio4.py
@@ -1,6 +1,6 @@
 #Calculadora simples
 
-#As duas linhas seguintes mostra uma mensagem para o usuário informar um número e aguarda a resposta.
+#As duas linhas seguintes mostra uma mensagem para o usuário informar dois números separadamente e aguarda a resposta.
 n1 = float(input("selecione número 1: "))
 n2 = float(input("selecione número 2: "))
 
