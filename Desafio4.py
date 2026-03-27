afd4442be38405c89d548e27701663a2b4b2c1c7
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-26 22:16:35 -0300

    Desafio4.py

--- Desafio4.py
+++ Desafio4.py
@@ -7,19 +7,19 @@ n2 = float(input("selecione número 2: "))
 #Mostra mensagem para o usuário escolher a operação.
 operação = input("escolha uma operação [soma, subtração, multiplicação ou divisão]: ")
 
-#Se for escolhido soma, o Pynthon soma n1 e n2 e mostra o resultado na tela (print)
+#Se for escolhido soma, o Pynthon soma n1 e n2 e mostra o resultado na tela.
 if operação == "soma":
   print( n1 + n2)
 
-#Se for escolhido subtração, o Pynthon subtrai n1 e n2 e mostra o resultado na tela (print)
+#Se for escolhido subtração, o Pynthon subtrai n1 e n2 e mostra o resultado na tela.
 elif operação == "subtração":
   print( n1 - n2)
 
-#Se for escolhido multiplicação, o Pynthon multiplica n1 e n2 e mostra o resultado na tela (print)
+#Se for escolhido multiplicação, o Pynthon multiplica n1 e n2 e mostra o resultado na tela.
 elif operação == "multiplicação":
   print( n1 * n2)
 
-#Se for escolhido divisão, o Pynthon divide n1 e n2 e mostra o resultado na tela (print)
+#Se for escolhido divisão, o Pynthon divide n1 e n2 e mostra o resultado na tela.
 elif operação == "divisão":
   print( n1 / n2)
 
