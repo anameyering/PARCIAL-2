56c6c3c62c81a4f9bd1a8cb4024815d7aaa1db00
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-26 22:23:39 -0300

    Desafio5.py

--- Desafio5.py
+++ Desafio5.py
@@ -1,6 +1,11 @@
 #Calcular área do triângulo
 
-base = float(input("qual a base de seu triangulo:"))
-altura = float(input("qual a altura de seu triangulo:"))
+#As 2 próximas linhas perguntam qual é o valor da base e altura do triangulo e agurdará a resposta.
+base = float(input("qual a base de seu triângulo:"))
+altura = float(input("qual a altura de seu triângulo:"))
+
+#Ao receber os dois valores, será feita a multiplicação da base e altura e depois será dividido por 2.
 area = (base * altura) / 2
-print(area)
+
+#Ao receber o resultado, será mostrado na tela.
+print("A área do triângulo é:", area)
