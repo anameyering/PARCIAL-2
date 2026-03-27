b636d175f50df1cbab435fa06b914efe8f0dd060
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-26 22:34:48 -0300

    Desafio1.py

--- Desafio1.py
+++ Desafio1.py
@@ -1,6 +1,6 @@
 #SOMA DE NÙMEROS
 
-#As duas linhas seguintes mostra uma mensagem para o usuário informar um número e aguarda a resposta.
+#As duas linhas seguintes mostra uma mensagem para o usuário informar dois números separadamente e aguarda a resposta.
 n1 = float(input('Informe número 1: '))
 n2 = float(input('Informe número 2: '))
 
