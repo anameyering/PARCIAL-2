8249bf7f857bcf34238d9b19a78ed4cba743d245
anameyering <ana.meyering@escola.pr.gov.br>
2026-03-31 09:09:48 -0300

    Desafio3.py

--- Desafio3.py
+++ Desafio3.py
@@ -1,6 +1,6 @@
 #LISTA NOMES
 
-#Mostra uma imagem dizendo para o usuário escrever os nomes e agurda a resposta, após receber os nomes, eles são divididos em partes, conforme o usuário separou por vírgulas.
+#Mostra uma mensagem dizendo para o usuário escrever os nomes e agurda a resposta, após receber os nomes, eles são divididos em partes, conforme o usuário separou por vírgulas.
 nomes = input("Digite os 5 nomes separados por vírgula: ").split(",")
 
 #Logo após, aparecerá a lista de nomes na tela. 
