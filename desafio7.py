28108c8c6555da9481cf75bd54d84fbc95bf4261
anameyering <ana.meyering@escola.pr.gov.br>
2026-04-07 13:40:23 -0300

    desafio7.py

--- desafio7.py
+++ desafio7.py
@@ -1,4 +1,4 @@
-#Formula dos juros
+#Fórmula dos juros
 
 #Mostra a mensagem, espera o usuário digitar o valor do capital e armazena o valor.
 capital = float(input("Digite seu capital (C): "))
@@ -9,8 +9,10 @@ taxa = float(input("Digite a taxa de juros (%): "))
 #Pergunta o tempo (meses) em que o dinheiro vai render juros.
 tempo = float(input("Digite o tempo em meses (T): "))
 
-#Calcula os juros simples usando a fórmula dos juros simples.
+#Calcula os juros simples usando a fórmula dos juros simples e o montante com a soma do juros e capital.
 juros = (capital*taxa*tempo) / 100
+montante = capital + juros
 
-#Mostra na tela o valor dos juros calculado.
+#Mostra na tela o valor dos juros calculado e o montante.
 print("O valor dos juros é", juros)
+print("O valor do montante é", montante)
